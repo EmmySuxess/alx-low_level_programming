bb8549d6d951cdcdc17866c8f6935fce183ae495
EmmySuxess <emmanuelsuxess@gmail.com>
2023-02-17 14:17:48 -0800

    Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - main function
+ *Return: 0 when successful
+ */
+int main(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+	putchar(letter);
+	letter++;
+}
+putchar('\n');
+return (0);
+}
+
