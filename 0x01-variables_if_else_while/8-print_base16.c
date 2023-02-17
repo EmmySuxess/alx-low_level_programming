0fc3c4f66fb4ac2f826bdec20aa9416fb4957f83
EmmySuxess <emmanuelsuxess@gmail.com>
2023-02-17 15:43:53 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar('0' + i);
+	for (i = 0; i < 6; i++)
+		putchar('a' + i);
+	putchar('\n');
+	return (0);
+}
