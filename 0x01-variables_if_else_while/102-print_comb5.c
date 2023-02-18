a7a6b14c58d691e29036017d0cea682ac57d63dc
EmmySuxess <emmanuelsuxess@gmail.com>
2023-02-17 20:11:37 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i < 100; i++)
 	{
 		for (j = i + 1; j < 100; j++)
 		{
@@ -17,7 +17,7 @@ int main(void)
 			putchar(' ');
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
-			if (i == 98 && j == 99)
+			if (i != 98 || j != 99)
 			{
 				break;
 			}
