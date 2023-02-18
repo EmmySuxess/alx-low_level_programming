879cfbb0f41170c5d7d08990fbde2b2313be7f35
EmmySuxess <emmanuelsuxess@gmail.com>
2023-02-17 17:02:15 -0800

    Print Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,16 +7,18 @@
 
 int main(void)
 {
-	int i, j, k;
+	int i = 0;
 
-	for (i = 0; i < 10; i++)
-		for (j = i + 1; j < 10; j++)
-			for (k = j + 1; k < 10; k++)
-			{
-				putchar(i + '0');
-				putchar(j + '0');
-				putchar(k + '0');
-				putchar(k == 9 ? '\n' : ',');
-			}
+	while (i < 10)
+	{
+		putchar(i + '0');
+	if (i != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	i++;
+	}
+		putchar('\n');
 	return (0);
 }
