916487df3ba9e78cf444b8b5d7218438a8ac6407
EmmySuxess <emmanuelsuxess@gmail.com>
2023-02-22 03:02:58 -0800

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int prev = 1, curr = 2, next = 0, sum = 0;
+
+while (curr <= 4000000)
+{
+if (curr % 2 == 0)
+sum += curr;
+
+next = prev + curr;
+prev = curr;
+curr = next;
+}
+
+printf("%d\n", sum);
+
+return 0;
+}
