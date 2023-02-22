e8d9ffe276de256aa7e7f8c8a4e04698d9fade3e
EmmySuxess <emmanuelsuxess@gmail.com>
2023-02-22 02:35:40 -0800

    Multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,42 +1,43 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: the size of the times table
- *
- * Return: void
+ * print_times_table - prints the n times table, starting with 0.
+ * @n: number of times table
  */
 void print_times_table(int n)
 {
-int i, j, prod;
+int i, j, res;
 
-if (n >= 0 && n <= 15)
-{
+if (n < 0 || n > 15)
+return;
+
 for (i = 0; i <= n; i++)
 {
 _putchar('0');
 for (j = 1; j <= n; j++)
 {
-prod = i * j;
+res = i * j;
 _putchar(',');
 _putchar(' ');
-if (prod <= 9)
-_putchar(' ');
-if (prod <= 99)
-_putchar(' ');
-if (prod >= 100)
-{
-_putchar(prod / 100 + '0');
-_putchar((prod / 10 % 10) + '0');
-}
-else if (prod >= 10)
+if (res < 10)
 {
 _putchar(' ');
-_putchar(prod / 10 + '0');
+_putchar(' ');
+_putchar(res + '0');
 }
-_putchar(prod % 10 + '0');
+else if (res < 100)
+{
+_putchar(' ');
+_putchar(res / 10 + '0');
+_putchar(res % 10 + '0');
+}
+else
+{
+_putchar(res / 100 + '0');
+_putchar((res / 10) % 10 + '0');
+_putchar(res % 10 + '0');
+}
 }
 _putchar('\n');
-}
 }
 }
