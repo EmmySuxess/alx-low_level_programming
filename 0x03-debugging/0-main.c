eb155f40c3bd6c05e375c6e315241e0a921aa238
EmmySuxess <emmanuelsuxess@gmail.com>
2023-02-23 12:42:03 -0800

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 
 /**
 * main - tests function that prints if integer is positive or negative
-* Return: always 0
+* Return: 0
 */
 
 int main(void)
@@ -11,6 +11,7 @@ int i;
 
 i = 0;
 positive_or_negative(i);
+printf("%d", i);
 
 return (0);
 }
