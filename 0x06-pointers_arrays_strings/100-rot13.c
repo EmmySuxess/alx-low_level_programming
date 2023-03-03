dcd97cedac8f84c870dc970952128d03cc0b4dae
EmmySuxess <emmanuelsuxess@gmail.com>
2023-03-03 14:35:59 -0800

    8. rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,38 +1,30 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * rot13 - Encode a string using rot13 encryption
- * @str: Pointer to the string to be encoded
+ * rot13 - encoder rot13
+ * @s: pointer to string params
  *
- * Return: Pointer to the encoded string
+ * Return: *s
  */
-char *rot13(char *str)
-{
-char *encoded = str;
-char *rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-int i, j;
 
-for (i = 0; str[i] != '\0'; i++)
+char *rot13(char *s)
 {
-for (j = 0; j < 52; j++)
-{
-if (str[i] == rot13[j])
-{
-encoded[i] = rot13[(j + 13) % 52];
-break;
-}
-else if ((str[i] >= 'A' && str[i] < 'N') || (str[i] >= 'a' && str[i] < 'n'))
-{
-encoded[i] = str[i] + 13;
-break;
-}
-else if ((str[i] >= 'N' && str[i] <= 'Z') || (str[i] >= 'n' && str[i] <= 'z'))
-{
-encoded[i] = str[i] - 13;
-break;
-}
-}
-}
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-return (encoded);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
 }
